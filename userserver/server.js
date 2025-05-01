20017f28087e6fc6e074377300f33389a78b737b
BisekhKarki <karkidai79@gmail.com>
2025-05-01 16:21:46 +0545

    Updated

--- userserver/server.js
+++ userserver/server.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = 4000;
 const connect = require("./Database/Connect");
 const userRouter = require("./Routes/UserRoutes");
 const passport = require("passport");
@@ -21,14 +21,13 @@ const { scheduleUserEmails } = require("./lib/Automatic");
 
 const homeRouter = require("./Routes/HomeRoute");
 
-app.set("trust proxy", 1);
-const frontend_Url = process.env.FRONTEND_URL || "http://localhost:3000";
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 
 app.use(express.json());
 app.use(passport.initialize());
 app.use(
   cors({
-    origin: frontend_Url,
+    origin: FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
