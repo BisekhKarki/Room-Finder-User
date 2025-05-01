ba38e422ae147e06115c1bf6799c146340101b00
BisekhKarki <karkidai79@gmail.com>
2025-05-01 15:24:08 +0545

    Updated

--- client/constants/BaseUrl.ts
+++ client/constants/BaseUrl.ts
@@ -1,5 +1,6 @@
 export const base_url =
-  process.env.NEXT_PUBLIC_BACKEND_BASE_URL || "http://localhost:4000/api";
+  (process.env.NEXT_PUBLIC_BACKEND_BASE_URL as string) ||
+  "http://localhost:4000/api";
 export const tenant_base_url =
-  process.env.NEXT_PUBLIC_BACKEND_TENANT_URL ||
+  (process.env.NEXT_PUBLIC_BACKEND_TENANT_URL as string) ||
   "http://localhost:4000/api/tenants";
